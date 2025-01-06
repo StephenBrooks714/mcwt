d4484c8e699119e41bc3c51127e0b9847453f3bc
StephenBrooks714 <stephenbrooks714@gmail.com>
2025-01-06 16:11:28 -0600

    update

--- data/controllers/pages/homePage.js
+++ data/controllers/pages/homePage.js
@@ -1,5 +1,8 @@
-module.exports = (req, res) => {
+const EventsDB = require("../models/Events");
+module.exports = async (req, res) => {
+    const eventsData = await EventsDB.find().limit(1).sort({ _id: -1 });
     res.render("index", {
         title: "Mom Can We Talk",
+        eventsData
     })
 }
